1ec3ed8b6357a0e130126fe2216fe4c853c138f6
russel0719 <39179920+russel0719@users.noreply.github.com>
2022-12-12 09:59:12 +0900

    fix TransformerNet

--- model/TransformerNet.py
+++ model/TransformerNet.py
@@ -68,29 +68,30 @@ class TransformerNet(nn.Module):
         self.encoder.add_module('in1', nn.InstanceNorm2d(32, affine=True))
         self.encoder.add_module('relu1', nn.ReLU())
         
-        self.encoder.add_module('conv2', ConvLayer(32, 64, kernel_size=3, stride=2))
-        self.encoder.add_module('in2', nn.InstanceNorm2d(64, affine=True))
-        self.encoder.add_module('relu2', nn.ReLU())
+        #self.encoder.add_module('conv2', ConvLayer(32, 64, kernel_size=3, stride=2))
+        #self.encoder.add_module('in2', nn.InstanceNorm2d(64, affine=True))
+        #self.encoder.add_module('relu2', nn.ReLU())
         
-        self.encoder.add_module('conv3', ConvLayer(64, 128, kernel_size=3, stride=2))
-        self.encoder.add_module('in3', nn.InstanceNorm2d(128, affine=True))
-        self.encoder.add_module('relu3', nn.ReLU())
+        #self.encoder.add_module('conv3', ConvLayer(64, 128, kernel_size=3, stride=2))
+        #self.encoder.add_module('in3', nn.InstanceNorm2d(128, affine=True))
+        #self.encoder.add_module('relu3', nn.ReLU())
 
         # Residual layers
         self.residual = nn.Sequential()
         
         for i in range(5):
-            self.residual.add_module('resblock_%d' %(i+1), ResidualBlock(128))
+            #self.residual.add_module('resblock_%d' %(i+1), ResidualBlock(128))
+            self.residual.add_module('resblock_%d' %(i+1), ResidualBlock(32))
         
         # Upsampling Layers
         self.decoder = nn.Sequential()
-        self.decoder.add_module('deconv1', UpsampleConvLayer(128, 64, kernel_size=3, stride=1, upsample=2))
-        self.decoder.add_module('in4', nn.InstanceNorm2d(64, affine=True))
-        self.encoder.add_module('relu4', nn.ReLU())
+        #self.decoder.add_module('deconv1', UpsampleConvLayer(128, 64, kernel_size=3, stride=1, upsample=2))
+        #self.decoder.add_module('in4', nn.InstanceNorm2d(64, affine=True))
+        #self.encoder.add_module('relu4', nn.ReLU())
 
-        self.decoder.add_module('deconv2', UpsampleConvLayer(64, 32, kernel_size=3, stride=1, upsample=2))
-        self.decoder.add_module('in5', nn.InstanceNorm2d(32, affine=True))
-        self.encoder.add_module('relu5', nn.ReLU())
+        #self.decoder.add_module('deconv2', UpsampleConvLayer(64, 32, kernel_size=3, stride=1, upsample=2))
+        #self.decoder.add_module('in5', nn.InstanceNorm2d(32, affine=True))
+        #self.encoder.add_module('relu5', nn.ReLU())
 
         self.decoder.add_module('deconv3', ConvLayer(32, 3, kernel_size=9, stride=1))
 
